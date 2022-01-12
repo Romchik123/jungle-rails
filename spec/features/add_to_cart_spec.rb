3662f3c3f7e4d98226afbb7c7a621c51ff4643a1
rom <mozzieisme@gmail.com>
2022-01-12 09:24:41 -0500

    Add new file and modify the files

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
+    1.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -18,10 +18,9 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
   scenario "They see all products" do
     # ACT
     visit root_path
-    first("article.product").find_link("Details").click
-    
-    # VERIFY
-    expect(page).to have_css 'article.product-detail'
+    click_on 'Add'
+
+    expect(page).to have_content "My Cart (1)"
 
     # DEBUG
     save_screenshot
